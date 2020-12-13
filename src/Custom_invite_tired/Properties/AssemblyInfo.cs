338a53daa3106d0174cd24cec6db7d687b905bbb
Tired(Michael) <42284777+tireder@users.noreply.github.com>
2020-12-13 03:27:00 +0200

    Add files via upload

---
+++ src/Custom_invite_tired/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: Guid("5baaf77a-d479-44ce-9fa2-53af7a413e97")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Apple Loader RC")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Apple Loader RC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
